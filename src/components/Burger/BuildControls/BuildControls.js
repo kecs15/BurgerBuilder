82228adef1309cc4cceba7621b18bd23ae77eb1f
Kedin Cuevas <kedin.c@gmail.com>
2019-08-17 20:59:12 -0400

    Connecting Stato to Build Controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}    
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl =>
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}/>
+            )}
+    </div>
+);
+
+export default buildControls;
