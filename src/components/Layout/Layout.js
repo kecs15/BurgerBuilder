2072fc9d83cb961ed83cad498c4398c0e907c555
Kedin Cuevas <kedin.c@gmail.com>
2019-08-28 18:09:04 -0400

    Button component added for Modal

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <div>Toolbar, SideDrawer</div>
         <main className={classes.Content}>
             {props.children}
         </main>
