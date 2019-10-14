9752160763e2b761c349d641179896626d4452fd
Kedin Cuevas <kedin.g@gmail.com>
2019-10-14 09:58:19 -0400

    Extra validation added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,7 +93,7 @@ class ContactData extends Component {
                             {value: 'cheapest', displayValue: 'Cheapest'}
                         ]
                     },
-                    value: '',
+                    value: 'fastest',
                     validation: {},
                     valid: true
                 }
