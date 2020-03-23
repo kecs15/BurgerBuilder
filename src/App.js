2ed57b879fa6407fb71a32645003907236135d10
Kedin Cuevas <kedin.c@gmail.com>
2020-03-22 20:33:09 -0400

    Fixing Connect and Routing errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,21 +8,34 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div >
-      <Layout>
-        <Switch>
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/logout" exact component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSingup()
+  }
+  render() {
+    return (
+      <div >
+        <Layout>
+          <Switch>
+            <Route path="/auth" exact component={Auth} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/logout" exact component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSingup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
