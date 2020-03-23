ade479a1bf263cd7604c84661b3701c5bf905317
Kedin Cuevas <kedin.c@gmail.com>
2020-03-23 15:38:10 -0400

    Sharing Validation method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../utility/utility';
+import { updateObject, checkValidation } from '../../../utility/utility';
 
 class ContactData extends Component {
     state = {
@@ -119,41 +119,11 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidation(value, rules){
-        let isValid = true;
-        if(!rules) {
-            return true;
-        }
-        if(rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if(rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if(rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        
-        if (rules.isEmail) {            
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }        
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
         
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidation(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidation(event.target.value, this.state.orderForm[inputIdentifier].validation),
             touched: true
         });
 
